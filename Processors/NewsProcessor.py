9fc9939583b81b0699c00451fc009cfe28693b62
songyingxin <18310523922@163.com>
2019-04-30 21:50:54 +0800

    bertOrigin

--- Processors/NewsProcessor.py
+++ Processors/NewsProcessor.py
@@ -25,17 +25,41 @@ class NewsProcessor(DataProcessor):
 
     def get_labels(self):
         """See base class."""
-        return list(self.labels)
+        return ['房产', '科技', '财经', '游戏', '娱乐', '时尚', '时政', '家居', '教育', '体育']
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
         examples = []
         for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
             guid = "%s-%s" % (set_type, i)
-            text_a = tokenization.convert_to_unicode(line[1])
-            label = tokenization.convert_to_unicode(line[0])
-            self.labels.add(label)
+            text_a = line[0]
+            label = line[1]
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
 
         return examples
+
+
+def cnews_data(data_dir, tokenizer, processor, max_length, train_batch_size, dev_batch_size, test_batch_size):
+
+    train_examples = processor.get_train_examples(data_dir)
+    dev_examples = processor.get_dev_examples(data_dir)
+    test_examples = processor.get_test_examples(data_dir)
+    label_list = processor.get_labels()
+
+    train_features = convert_examples_to_features(
+        train_examples, label_list, max_length, tokenizer)
+    dev_features = convert_examples_to_features(
+        dev_examples, label_list, max_length, tokenizer)
+    test_features = convert_examples_to_features(
+        test_examples, label_list, max_length, tokenizer)
+
+    train_dataloader = convert_features_to_tensors(
+        train_features, train_batch_size)
+    dev_dataloader = convert_features_to_tensors(dev_features, dev_batch_size)
+    test_dataloader = convert_features_to_tensors(
+        test_features, test_batch_size)
+
+    return train_dataloader, dev_dataloader,  test_dataloader, label_list, len(train_examples)
--- run_CNews.py
+++ run_CNews.py
@@ -1,12 +1,13 @@
 import random
-import numpy as np 
+import numpy as np
 import time
 from tqdm import tqdm
 import os
 
 import torch
 import torch.nn as nn
-from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, TensorDataset) 
+from torch.utils.data import (
+    DataLoader, RandomSampler, SequentialSampler, TensorDataset)
 
 
 from pytorch_pretrained_bert.tokenization import BertTokenizer
@@ -16,7 +17,8 @@ from pytorch_pretrained_bert.optimization import BertAdam, warmup_linear
 from Utils import utils
 from BertOrigin import args
 
-from Processors.SST2Processor import Sst2Processor, sst_data
+from Processors.NewsProcessor import NewsProcessor, cnews_data
+
 
 def train(model, dataloader, optimizer, criterion, gradient_accumulation_steps, device, label_list):
     epoch_loss = 0
@@ -81,7 +83,7 @@ def evaluate(model, dataloader, criterion, device, label_list):
 
         label_ids = label_ids.to('cpu').numpy()
         all_labels = np.append(all_labels, label_ids)
-        
+
         epoch_loss += loss.mean().item()
 
     report, acc = utils.classifiction_metric(all_preds, all_labels, label_list)
@@ -92,11 +94,11 @@ def main(config):
 
     if not os.path.exists(config.output_dir):
         os.makedirs(config.output_dir)
-    
+
     if not os.path.exists(config.cache_dir):
         os.makedirs(config.cache_dir)
-        
-    output_model_file = os.path.join(config.output_dir, WEIGHTS_NAME) # 模型输出文件
+
+    output_model_file = os.path.join(config.output_dir, WEIGHTS_NAME)  # 模型输出文件
     output_config_file = os.path.join(config.output_dir, CONFIG_NAME)
 
     device, n_gpu = utils.get_device()  # 设备准备
@@ -111,18 +113,20 @@ def main(config):
         torch.cuda.manual_seed_all(config.seed)
 
     """ 数据准备 """
-    processor = Sst2Processor()  # processor 选择
+    processor = NewsProcessor()  # processor 选择
     tokenizer = BertTokenizer.from_pretrained(
-        "/home/songyingxin/datasets/pytorch-bert/vocabs/bert-base-uncased-vocab.txt", do_lower_case=config.do_lower_case)  # 分词器选择
+        "/home/songyingxin/datasets/pytorch-bert/vocabs/bert-base-chinese-vocab.txt", do_lower_case=config.do_lower_case)  # 分词器选择
 
-    train_dataloader, dev_dataloader, test_dataloader, label_list, train_examples_len = sst_data(config.data_dir, tokenizer, processor, config.max_seq_length, config.train_batch_size, config.dev_batch_size, config.test_batch_size)
+    train_dataloader, dev_dataloader, test_dataloader, label_list, train_examples_len = cnews_data(
+        config.data_dir, tokenizer, processor, config.max_seq_length, config.train_batch_size, config.dev_batch_size, config.test_batch_size)
 
     num_labels = len(label_list)
-    num_train_optimization_steps = int(train_examples_len / config.train_batch_size / config.gradient_accumulation_steps) * config.num_train_epochs 
+    num_train_optimization_steps = int(
+        train_examples_len / config.train_batch_size / config.gradient_accumulation_steps) * config.num_train_epochs
 
     # 模型准备
     model = BertForSequenceClassification.from_pretrained(
-        "/home/songyingxin/datasets/pytorch-bert/bert-base-uncased", cache_dir=config.cache_dir, num_labels=num_labels)
+        "/home/songyingxin/datasets/pytorch-bert/bert-base-chinese", cache_dir=config.cache_dir, num_labels=num_labels)
     model.to(device)
 
     # 优化器
@@ -143,19 +147,21 @@ def main(config):
     criterion = nn.CrossEntropyLoss()
     criterion = criterion.to(device)
 
-
     best_dev_loss = float('inf')
     for epoch in range(int(config.num_train_epochs)):
         start_time = time.time()
-        train_loss, train_acc, train_report = train(model, train_dataloader, optimizer, criterion, config.gradient_accumulation_steps, device, label_list)
+        train_loss, train_acc, train_report = train(
+            model, train_dataloader, optimizer, criterion, config.gradient_accumulation_steps, device, label_list)
 
-        dev_loss, dev_acc, dev_report = evaluate(model, dev_dataloader, criterion, device, label_list)
+        dev_loss, dev_acc, dev_report = evaluate(
+            model, dev_dataloader, criterion, device, label_list)
 
         end_time = time.time()
 
         epoch_mins, epoch_secs = utils.epoch_time(start_time, end_time)
 
-        print(f'---------------- Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s ----------')
+        print(
+            f'---------------- Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s ----------')
         print("-------------- Train -------------")
         print(f'\t \t Loss: {train_loss:.3f} |  Acc: {train_acc*100: .2f} %')
         print(train_report)
@@ -167,11 +173,12 @@ def main(config):
         if dev_loss < best_dev_loss:
             best_dev_loss = dev_loss
 
-            model_to_save = model.modules if hasattr(model, 'module') else model
+            model_to_save = model.modules if hasattr(
+                model, 'module') else model
             torch.save(model_to_save.state_dict(), output_model_file)
             with open(output_config_file, 'w') as f:
                 f.write(model_to_save.config.to_json_string())
-    
+
     bert_config = BertConfig(output_config_file)
     model = BertForSequenceClassification(bert_config, num_labels=num_labels)
     model.load_state_dict(torch.load(output_model_file))
@@ -185,10 +192,9 @@ def main(config):
     print(test_report)
 
 
-
 if __name__ == "__main__":
 
-    data_dir = "/home/songyingxin/datasets/SST-2"
+    data_dir = "/home/songyingxin/datasets/cnews"
     output_dir = ".output"
     cache_dir = ".cache"
     main(args.get_args(data_dir, output_dir, cache_dir))
